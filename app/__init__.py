8cad07fe5e0bb2183f12df34dadce9300f1ec38f
yatskivbohdan <44812624+yatskivbohdan@users.noreply.github.com>
2020-06-10 02:51:56 +0300

    Updated database server

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,10 @@ from sqlalchemy import create_engine
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'you-will-never-guess'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:liner@localhost/rent_friends'
+app.config['SECRET_KEY'] = 'student'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:student@localhost/friend_rent'
 
-engine = create_engine('postgresql://postgres:liner@localhost/rent_friends')
+engine = create_engine('postgresql://postgres:student@localhost/friend_rent')
 conn = engine.connect()
 
 from app import routes
